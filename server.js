12ba348dc7dd6998f96d1a4402d108ae139cfc5c
leggan <lovegu2007@gmail.com>
2025-03-07 04:40:55 +0100

    1
    1

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import passport from 'passport'
+import initializePassport from './config/passportConfig.js';
+
+initializePassport(passport)
+
+const app = express()
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false})) 
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name : 'Kyle'})
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res, next) => {
+
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
+
+
+app.listen(3000)
