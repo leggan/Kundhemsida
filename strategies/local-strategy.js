67195e178a0e68f967c24ee8f156fb839b6a53d7
love12341234 <love.ejdervikgustafsson@elev.ga.ntig.se>
2025-03-13 11:17:46 +0100

    1
    1

--- strategies/local-strategy.js
+++ strategies/local-strategy.js
@@ -9,25 +9,41 @@ const configurePassport = async () => {
     passport.use(
         new Strategy(async(username, password, done) => {
             if(!username  || !password) {
-                return done(error, false)
+                return done()
             } else {
                 const findUser = await User.findOne({username: username}) 
-                console.log(`FindUser PSW: ${findUser.password}`)
+                console.log(`FindUser PSW: ${findUser}`)
                 if(findUser) {
                     try {
                         const match = await bcrypt.compare(password, findUser.password)
                         if(match) {
                             return done(null, true)
                         } else {
-                            return done(true)
+                            return done('Wrong password', false)
                         }
                     } catch(error) {
                         return done(error, false)
                     }
-                } 
+                } else {
+                    console.log('User doesnt exists!')
+                    return done(null, false)
+                }
             }
         })
     )
+    passport.serializeUser((findUser, done) => {
+        console.log(findUser)
+        done(null, findUser.id)
+    })
+    passport.serializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id)
+            done(null, user)
+        }
+        catch(error) {
+            console.log(error)
+        }
+    })
 }
 
 export default configurePassport
