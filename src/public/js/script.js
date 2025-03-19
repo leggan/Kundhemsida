318c237f3b4675d6d6cfc630b00d91aecedf10f6
love12341234 <love.ejdervikgustafsson@elev.ga.ntig.se>
2025-03-19 13:54:14 +0100

    1
    1

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -12,4 +12,24 @@ document.addEventListener("DOMContentLoaded", () => {
             profileDropdown.classList.remove("show");
         }
     });
+
+    let index = 0;
+    const slides = document.querySelectorAll(".dishes");
+
+    function changeSlide() {
+        slides[index].classList.remove("dishes-active"); // Ta bort aktiv från nuvarande slide
+
+        index = (index + 1) % slides.length; // Nästa slide
+
+        slides[index].style.left = "100%"; // Startposition utanför till höger
+        setTimeout(() => {
+            slides[index].classList.add("dishes-active"); // Lägg till active på nästa slide
+            slides[index].style.left = "0"; // Flytta in den nya sliden
+        }, 50);
+    }
+
+    // Byt slide var tredje sekund
+    setInterval(changeSlide, 3000);
+
+
 });
